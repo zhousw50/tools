ed43850d24a2902951ce78a35c6c6e8edaa5f1d9
周声文 <73733215+zhousw50@users.noreply.github.com>
2022-03-12 23:35:30 +0800

    Add files via upload

--- 解二元一次方程组/index.js
+++ 解二元一次方程组/index.js
@@ -1,3 +1,23 @@
+function check() {
+    let a11= document.getElementById("a11").value;
+    let a12= document.getElementById("a12").value;
+    let a21= document.getElementById("a21").value;
+    let a22= document.getElementById("a22").value;
+    let b1= document.getElementById("b1").value;
+    let b2= document.getElementById("b2").value;
+    if(a11&&a12&&a21&&a22&&b1&&b2)
+    {
+        var res = verifyCode.validate(code.value);
+        if (res) {
+            count();
+        }
+    }
+    else
+    {
+        document.getElementById("ans-card").setAttribute("hidden","true");
+        document.getElementById("ans-card2").setAttribute("hidden","true")
+    }
+}
 function count(){
     let a11= document.getElementById("a11").value;
     let a12= document.getElementById("a12").value;
@@ -16,29 +36,30 @@ function count(){
         let y=(b2-a21/a11*b1)/(a22-a21/a11*a12)
         document.getElementById("ans-card").removeAttribute("hidden");
         document.getElementById("ans-card2").removeAttribute("hidden");
-        let ans= '<img src="https://latex.codecogs.com/svg.image?'+
+        let ans= '\\('+
             '\\'+'begin{cases}'+
             'x='+x+'\\'+'\\'+
             'y='+y+
-            '\\'+'end{cases}\"'+
-            'title="\\begin{cases}x=1\\\\y=2\\end{cases}" />';
+            '\\'+'end{cases}\\)';
         if (ans=="x=NaN\ny=NaN") document.getElementById('ans').innerText="有无数个解";
         else if (ans=="x=-Infinity\ny=Infinity"||ans=="x=Infinity\ny=-Infinity") document.getElementById('ans').innerText="无解";
         else {
             document.getElementById('ans').innerHTML=ans;
             document.getElementById("ans-card2").removeAttribute("hidden");
-            document.getElementById('1').innerText="① × "+a21+"得"+a21*a11+"  x +"+a21*a12+" y = "+b1*a21+" ③";
-            if(a21*a12<0) document.getElementById('1').innerText="① × "+a21+"得"+a21*a11+" x "+a21*a12+" y = "+b1*a21+" ③";
-            else document.getElementById('1').innerText="① × "+a21+"得"+a21*a11+" x + "+a21*a12+" y = "+b1*a21+" ③";
-            if(a22*a11<0)document.getElementById('2').innerText="② × "+a11+"得"+a21*a11+" x "+a22*a11+" y = "+b2*a11+" ④";
-            else document.getElementById('2').innerText="② × "+a11+"得"+a21*a11+" x + "+a22*a11+" y = "+b2*a11+" ④";
-            document.getElementById('3').innerText="④ - ③ 得"+" y="+y;
-            document.getElementById('4').innerText="把 y = "+y+" 代入①得"+" x = "+x;
-            document.getElementById('5').innerHTML="∴ 原方程组的解为";
-            document.getElementById('6').innerHTML=ans;
+            var ans2;
+            if(a21*a12<0) ans2="\\(\\mbox{解：①}\\times"+a21+"\\mbox{得}\\)\\("+a21*a11+"x"+a21*a12+"y="+b1*a21+"\\mbox{ ③}\\)"+"<br>";
+            else ans2="\\(\\mbox{解：①}\\times"+a21+"\\mbox{得}\\)\\("+a21*a11+"x+"+a21*a12+"y="+b1*a21+"\\mbox{ ③}\\)"+"<br>";
+            if(a22*a11<0)ans2=ans2+"\\(\\mbox{②}\\times"+a11+"\\mbox{得}\\)\\("+a21*a11+"x"+a22*a11+"y="+b2*a11+"\\mbox{ ④}\\)"+"<br>";
+            else ans2=ans2+"\\(\\mbox{②}\\times"+a11+"\\mbox{得}\\)\\("+a21*a11+"x+"+a22*a11+"y="+b2*a11+"\\mbox{ ④}\\)"+"<br>";
+            ans2=ans2+"\\(\\mbox{④}-\\mbox{③} \\mbox{得}"+" y="+y+"\\)<br>";
+            ans2=ans2+"\\(\\mbox{把}y="+y+"\\mbox{代入①得}\\)"+"\\(x="+x+"\\)<br>";
+            ans2=ans2+"\\(\\therefore  \\mbox{原方程组的解为}\\)";
+            ans2=ans2+ans;
+            document.getElementById('ans2').innerHTML=ans2;
+            MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
             }
         }
-}
+    }
 function count2() {
     let a=document.getElementById('a').value;
     let b=document.getElementById('b').value;
